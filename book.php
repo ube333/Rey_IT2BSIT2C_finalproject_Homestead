3c8d4257deb901f8dcd19ddef7909eed726ea8fc
ube333 <rubylyn.rey001@gmail.com>
2023-05-27 00:54:49 +0800

    designing index

---
+++ book.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php include 'design/navbar.php'?>
+	<p class="br"></p>
+	<?php include 'design/book_container.php'?>
+	<p class="br"></p>
+	<?php include 'design/footer.php'?>
+</body>
+</html>
