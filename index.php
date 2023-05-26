d79ad22c4476d20a4db5e7a6ef0b2d047722a4f2
ube333 <rubylyn.rey001@gmail.com>
2023-05-27 00:42:38 +0800

    initializing index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MGB</title>
+</head>
+<body>
+	<?php include 'design/navbar.php'?>
+	<?php include 'design/home.php' ?>
+	<?php include 'design/home_content.php' ?>
+	<?php include 'design/footer.php' ?>
+</body>
+</html>
